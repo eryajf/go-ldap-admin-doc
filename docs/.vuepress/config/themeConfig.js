d8ec97caa60703732b24bf14cb74d87cb177a4dd
二丫讲梵 <eryajf@163.com>
2022-06-24 16:11:38 +0800

    调整一些问题

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -9,6 +9,7 @@ module.exports = {
   repo: 'eryajf/go-ldap-admin', // 导航栏右侧生成Github链接
   searchMaxSuggestions: 10, // 搜索结果显示最大数
   lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
+  docsRepo: 'eryajf/go-ldap-admin-doc',
   editLinks: true, // 启用编辑链接
   docsDir: 'docs', // 编辑的文件夹
   docsBranch: 'main', // 编辑的分支
