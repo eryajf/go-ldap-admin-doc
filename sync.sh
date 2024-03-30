15812edf7e602360ce6ebdd9b6ccfe44aabfec66
eryajf <eryajf@163.com>
2024-03-30 11:04:21 +0800

    调整一些内容

--- sync.sh
+++ sync.sh
@@ -6,7 +6,7 @@ set -e
 yarn build
 # 同步
 rsync -az -e 'ssh -p 2023' --progress  --delete docs/.vuepress/dist/  \
-    root@8.136.215.57:/data/www/ldapdoc.eryajf.net/
+    root@prod2.eryajf.net:/data/www/ldapdoc.eryajf.net/
 
 # 更新检索的索引数据
-ssh -p 2023 root@8.136.215.57 'bash /data/docker/meilisearch/scraper/ldapdoc.sh'
+ssh -p 2023 root@prod2.eryajf.net 'bash /data/docker/meilisearch/scraper/ldapdoc.sh'
